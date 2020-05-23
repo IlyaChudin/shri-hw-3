c512c58953b72e55c7e45fd8259ee2d9ff27912d
Ilya Chudin <chudin.ilya@gmail.com>
2020-05-23 13:36:30 +0400

    add react-i18next
    add translations for footer component

--- packages/client/src/index.tsx
+++ packages/client/src/index.tsx
@@ -1,17 +1,21 @@
-import React from "react";
+import React, { Suspense } from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import "./App.scss";
 import createStore from "./store";
+import "./i18n";
+import Loading from "./components/Loading";
 
 const store = createStore();
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <Suspense fallback={<Loading />}>
+        <App />
+      </Suspense>
     </Provider>
   </React.StrictMode>,
   document.getElementById("root")
